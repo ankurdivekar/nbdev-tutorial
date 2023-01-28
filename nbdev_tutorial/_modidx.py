27fcb2869129dc830c0a315dc350ccefa5cb2374
Ankur Divekar <ankur.divekar@gmail.com>
2023-01-28 16:55:21 +0530

    Test commit

--- nbdev_tutorial/_modidx.py
+++ nbdev_tutorial/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://ankurdivekar.github.io',
                 'git_url': 'https://github.com/ankurdivekar/nbdev-tutorial',
                 'lib_path': 'nbdev_tutorial'},
-  'syms': {'nbdev_tutorial.core': {'nbdev_tutorial.core.foo': ('core.html#foo', 'nbdev_tutorial/core.py')}}}
+  'syms': { 'nbdev_tutorial.core': {'nbdev_tutorial.core.foo': ('intro.html#foo', 'nbdev_tutorial/core.py')},
+            'nbdev_tutorial.introduction': { 'nbdev_tutorial.introduction.foo': ( 'introduction.html#foo',
+                                                                                  'nbdev_tutorial/introduction.py')}}}
